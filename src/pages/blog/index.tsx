407c123111a9dd0cb16874d791948f9877cd5bd0
bambanggunawanid <bambanggunawan887@gmail.com>
2023-08-08 09:54:44 +0700

    feat: add blog pages

---
+++ src/pages/blog/index.tsx
@@ -0,0 +1,9 @@
+export default function Blog() {
+  return (
+    <>
+      <div className="flex h-screen justify-center items-center">
+        <h1 className="text-xl font-bold">Blog</h1>
+      </div>
+    </>
+  );
+}
